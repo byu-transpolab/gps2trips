e4cf2d60d022a36e89d5dfa12c027a6cca62cd04
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-04-13 16:45:09 -0600

    Add skeleton for gps2trips function

---
+++ R/gps2trips.R
@@ -0,0 +1,15 @@
+#' Function to read GPS points into trips
+#'
+#' @param df A dataframe containing a stream of GPS points
+#'
+#'
+#' @examples
+#' # examples of how to use this function
+#'
+#' @export
+#'
+gps2trips <- function(df) {
+
+
+  # everything this function does goes in here.
+}
