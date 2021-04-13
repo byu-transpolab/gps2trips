1e169570c62cdbd51c6688c511ff23ddd22daf0a
Greg Macfarlane <gregmacfarlane@gmail.com>
2021-04-13 16:48:57 -0600

    Add x and y parameters to gps2trips function

--- R/gps2trips.R
+++ R/gps2trips.R
@@ -1,6 +1,8 @@
 #' Function to read GPS points into trips
 #'
 #' @param df A dataframe containing a stream of GPS points
+#' @param x The name of the x-coordinate variable in df
+#' @param y The name of the y-coordinate variable in df
 #'
 #'
 #' @examples
@@ -8,7 +10,7 @@
 #'
 #' @export
 #'
-gps2trips <- function(df) {
+gps2trips <- function(df, x = "x", y = "y") {
 
 
   # everything this function does goes in here.
