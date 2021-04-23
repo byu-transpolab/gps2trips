3440ec789b28b5df59dbdcd9e45e73c7f3f986f2
unknown <martingillian4@gmail.com>
2021-04-23 16:24:40 -0600

    separates timestamps into two columns of time and date

--- R/gps2trips.R
+++ R/gps2trips.R
@@ -14,4 +14,29 @@ gps2trips <- function(df, x = "x", y = "y") {
 
 
   # everything this function does goes in here.
+
+library(lubridate)
+library(tidyverse)
+
+  # the following lines convert the timestamp column into separate date and time columns
+
+
+  individualDataSet <- read.csv("C:/Users/Gillian/Downloads/Health Study_5f5184e73e2fd848eac22aec_passivelocation_65.csv")
+  timeanddateValue <- individualDataSet[,8]
+  convertDateAndTime <- data.frame(GivenFormat=c(timeanddateValue))
+
+  convertDateAndTime %>%
+  separate(GivenFormat, into = c("Date", "Time"), sep = " ", remove = TRUE) %>%
+  mutate(Date = lubridate::as_date(Date,format= "%Y-%m-%d"),
+         Time = hms::as_hms(str_c(Time, ":00")))
+
+
+  # end of date and time column code
+
+  # continue the function below
+  # do we want to insert these columns back onto the CSV file? where to go from here?
+
 }
+
+
+
