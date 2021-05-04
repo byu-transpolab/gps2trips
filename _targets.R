ae69bc242dc584042b13f78c1577d9fafcb2aef5
unknown <martingillian4@gmail.com>
2021-05-04 14:18:36 -0600

    Created Target script for gps2trips

---
+++ _targets.R
@@ -0,0 +1,26 @@
+library(targets)
+# This is an example _targets.R file. Every
+# {targets} pipeline needs one.
+# Use tar_script() to create _targets.R and tar_edit()
+# to open it again for editing.
+# Then, run tar_make() to run the pipeline
+# and tar_read(summary) to view the results.
+
+# Define custom functions and other global objects.
+# This is where you write source(\"R/functions.R\")
+# if you keep your functions in external scripts.
+
+source("R/gps2trips.R")
+
+options(dplyr.summarise.inform = FALSE)
+
+# Set target-specific options such as packages.
+tar_option_set(packages = c("tidyverse","lubridate","hms","dplyr"))
+
+# End this file with a list of target objects.
+list(
+
+  # Convert GPS Points into Trips
+  tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
+  tar_target(summary, summ(data)) # Call your custom functions as needed.
+)
