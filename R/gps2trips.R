4ac0addf738acb01b87658409aa539b1310aa8bc
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-05-18 10:58:08 -0600

    Clean up intial functions
    Some re-linting but also use sf to get distances.

--- R/gps2trips.R
+++ R/gps2trips.R
@@ -1,3 +1,12 @@
+# everything this function does goes in here.
+# load the necessary packages
+library(lubridate)
+library(tidyverse)
+library(hms)
+library(dplyr)
+library(sf)
+library(leaflet)
+
 #' Function to read GPS points into trips
 #'
 #' @param df A dataframe containing a stream of GPS points
@@ -12,44 +21,42 @@
 #'
 gps2trips <- function(df, x = "x", y = "y") {}
 
-  # everything this function does goes in here.
-  # install the necessary packages
-
-library(lubridate)
-library(tidyverse)
-library(hms)
-library(dplyr)
 
 #' Calculate the distance the person traveled using the latitude and longitude
 #' values and Halversine formula
 #' @param two latitude and two longitude values from the separated date and
 #' time tibble as created by 'separateDateandTime()'
 #' @return distance traveled in meters
-
-  distanceTraveled <- function(lat,lon,lat1,lon1) {
-    R <- 6371 # Earth mean radius (km)
-    delta.lon <- (lon1-lon)
-    delta.lat <- (lat1-lat)
-    a <- sin(delta.lat/2)^2 + cos(lat) * cos(lat1) *sin(delta.lon/2)^2
-    c <- 2 * a*sin(min(1,sqrt(a)))
-    d <- R * c * 1000
-    return (d) # Distance in m
-  }
+distanceTraveled <- function(lat,lon,lat1,lon1) {
+  R <- 6371 # Earth mean radius (km)
+  delta.lon <- (lon1-lon)
+  delta.lat <- (lat1-lat)
+  a <- sin(delta.lat/2)^2 + cos(lat) * cos(lat1) *sin(delta.lon/2)^2
+  c <- 2 * a*sin(min(1,sqrt(a)))
+  d <- R * c * 1000
+  return (d) # Distance in m
+}
 
 #' @param raw_file Path to raw file in local directory
 #' @return A tibble with raw gps data
 
-  getData <- function(raw_file) {
-    read_csv(raw_file)
-  }
+getData <- function(raw_file) {
+  read_csv(raw_file)
+}
 
 #' Separate date and time into separate columns
 #' @param Raw GPS data as read in with 'getData()'
 #' @return A clean tibble with only selected variables
+cleanData <- function(raw_data) {
 
-  cleanData <- function(raw_data) {
-    raw_data %>%
+  # empty coordinate point to use for lead/lag distances
+  empty <- st_as_sfc("POINT(EMPTY)", crs = 4326)
+
+   raw_data %>%
+    group_by(userId) %>%
     arrange(timestamp) %>%
+
+    # clean up times as lubridate objects
     mutate(
       Date = lubridate::date(timestamp),   # Separate Date and Time columns
       hour = lubridate::hour(timestamp),
@@ -57,21 +64,21 @@ library(dplyr)
       second = lubridate::second(timestamp),
       Time = hms::as_hms(str_c(hour, minute, second, sep = ":")),
     ) %>%
-     # group_by(Date, minute) %>%
-    #sample_frac(.1) %>%
-      group_by(Date) %>%
-      arrange(Time) %>%
-      mutate(
-        lat1=lead(lat),
-        lon1 = lead(lon),
-        TimeDifference = lead(Time)-Time # Time difference between each GPS data point
-      ) %>%
-    rowwise %>%
-    mutate (
-      distance_Meters = distanceTraveled(lat,lon,lat1,lon1), # Distance in meters between in each GPS data point
-      actual_speed = distance_Meters/as.integer(TimeDifference)  # Speed at each GPS data point
-    ) %>%
-    select(userId,deviceId,Date,Time,lat,lon,distance_Meters,TimeDifference,actual_speed)  # Select variables we want
+
+    # convert to SF data frame
+    st_as_sf(coords = c("lon", "lat"), crs = 4326)  %>%
+
+    # calculate elapsed time and distances
+    transmute(
+      userId,
+      timestamp,
+      TimeDifference = lead(timestamp)- timestamp, # Time difference between each GPS data point
+      distance_Meters = sf::st_distance(
+        geometry, lead(geometry, default = empty),
+        by_element = TRUE),
+      speed = distance_Meters / as.numeric(TimeDifference),
+      geometry
+    )
 }
 
 plotData <- function(cleaned_data) {
