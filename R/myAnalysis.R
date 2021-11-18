ae6e5d705ab13adc657360e388403c5d7f34327f
Gillian Riches <81657058+gillianriches@users.noreply.github.com>
2021-11-18 13:47:11 -0700

    Add base map

--- R/myAnalysis.R
+++ R/myAnalysis.R
@@ -1,28 +1,39 @@
-library(tidyverse)
-library(lubridate)
-library(dbscan)
-library(sf)
-library(leaflet)
-library(targets)
+source("R/gps2trips.R")
 
-tar_make()
+library(tidyverse)
+library(targets)
+library(ggspatial)
+
 tar_load(clusters_per_date)
 
-clusters_per_date <- clusters_per_date %>%
-  dplyr::filter(date == date)
+# How to add a Base Map to this?
 
-maps_per_date <- clusters_per_date %>%
-  mutate(
-    map = map2(data, clusters, ~ ggplot()
-               + geom_sf(data = .x, color = "blue")
-               + geom_sf(data = .y, color = "green", size = 8)
-               + labs(
-                 title = "Trips made on",
-                 subtitle = date,
-                 x = "Longitude",
-                 y = "Latitude",
-               )
+makeMaps <- function(clusters_per_date) {
+  clusters_per_date <- clusters_per_date %>%
+    dplyr::filter(date == date)
+  maps_per_date <- clusters_per_date %>%
+    mutate(
+      map = map2(data, clusters, ~ ggplot() +
+                   annotation_map_tile(type = "cartolight", zoom = 12) +
+                   theme(
+                     axis.line = element_line(color = NA),
+                     axis.title.x = element_blank(),
+                     axis.title.y = element_blank(),
+                     axis.ticks.x = element_blank(),
+                     axis.ticks.y = element_blank(),
+                     axis.text.x = element_blank(),
+                     axis.text.y = element_blank()
+                   )
+                 + geom_sf(data = .x, color = "blue")
+                 + geom_sf(data = .y, color = "green", size = 8, alpha = 0.3)
+                 + labs(
+                   title = "Trips made on",
+                   subtitle = date,
+                   x = "Longitude",
+                   y = "Latitude",
+                 ))
     )
-  )
+}
 
-print(maps_per_date$map)
+#print(maps_per_date$map)
+
