dd52e117be00f3fd46b10225c0ce4f6de52ef1fa
Gillian Riches <81657058+gillianriches@users.noreply.github.com>
2021-11-18 13:46:38 -0700

    Up to date with laptop now

--- R/gps2trips.R
+++ R/gps2trips.R
@@ -6,21 +6,46 @@
 # a single individual. Let's read all the CSV files in that folder
 
 makeCaps <- function(folder) {
-  files_in_folder <- dir(folder, full.names = TRUE)
+  files_in_folder <- unzip(folder, list = TRUE)$Name
   caps <- lapply(files_in_folder, function(x){
-  readr::read_csv(x, col_types = list(userId = col_character())) %>%
-    dplyr::transmute(
-      id = userId,
-      lat, lon,
-      timestamp,
-      date = lubridate::date(timestamp),   # Separate Date and Time columns
-      hour = lubridate::hour(timestamp),
-      minute = lubridate::minute(timestamp),
-      second = lubridate::second(timestamp),
-      time = hms::as_hms(str_c(hour, minute, second, sep = ":")),
-    ) %>% select(-hour, -minute, -second)
-}) %>%
-  dplyr::bind_rows()
+    readr::read_csv(unz(folder,x), col_types = list(userId = col_character())) %>%
+      dplyr::transmute(
+        id = userId,
+        lat, lon,
+        timestamp,
+        date = lubridate::date(timestamp),   # Separate Date and Time columns
+        hour = lubridate::hour(timestamp),
+        minute = lubridate::minute(timestamp),
+        second = lubridate::second(timestamp),
+        time = hms::as_hms(str_c(hour, minute, second, sep = ":")),
+      ) %>% select(-hour, -minute, -second)
+  }) %>%
+    dplyr::bind_rows() %>%
+    mutate(
+      activityDay = yesterday(timestamp)
+    )
+}
+
+#' Function to compute meaningful day
+#'
+#'
+#' @param timestamp vector of timestamp
+#' @return the day number of the timestamp
+#' @details if a timestamp occurs during midnight or 3 AM, it will be assigned to the
+#' previous calendar date
+
+yesterday <- function(timestamp){
+  x <- case_when(
+    lubridate::hour(timestamp)< 3~lubridate::day(timestamp)-1L,
+    lubridate::hour(timestamp)< 3 & lubridate::month(timestamp) %in% c(10,5,7,12) & lubridate::day(timestamp) == 1 ~ 30L,
+    lubridate::hour(timestamp)< 3 & lubridate::month(timestamp) %in% c(2,4,6,8,9,11,1) & lubridate::day(timestamp) == 1 ~ 31L,
+    lubridate::hour(timestamp)< 3 & lubridate::month(timestamp) ==3 & lubridate::day(timestamp) == 1 ~ 28L,
+    lubridate::hour(timestamp)< 3 & lubridate::month(timestamp) ==3 & lubridate::day(timestamp) == 1 & lubridate::leap_year(timestamp) ~ 29L,
+    TRUE ~ lubridate::day(timestamp)
+  )
+
+  str_c(x, lubridate::month(timestamp), sep = "-")
+
 }
 
 make_sf <- function(df) {
